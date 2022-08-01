475f84d43170f9bdccd9a8d8357353b47d335036
Joseph Collicoat <jcollicoat@gmail.com>
2022-08-01 17:47:56 +1200

    Delete commented code

--- pages/api/spotify/topTracks.ts
+++ pages/api/spotify/topTracks.ts
@@ -7,17 +7,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const endpoint = 'https://api.spotify.com/v1/me/top/tracks';
     const { access_token } = await getAccessToken();
 
-    /*
-    const response = await fetch(endpoint, {
-        headers: {
-            Authorization: `Bearer ${access_token}`,
-        },
-    });
-    const data = await response.json();
-
-    res.status(200).json({ data });
-    */
-
     const { data } = await axios.get(endpoint, {
         headers: {
             Authorization: `Bearer ${access_token}`,
