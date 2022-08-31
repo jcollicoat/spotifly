3e2e1abf448214506ba5aacfd85f449cb7e6f48e
Joseph Collicoat <jcollicoat@gmail.com>
2022-08-31 23:22:48 +1200

    Add react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+
+const queryClient = new QueryClient();
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const MyApp = ({
@@ -9,7 +13,10 @@ const MyApp = ({
 }: AppProps) => {
     return (
         <SessionProvider session={session}>
-            <Component {...pageProps} />
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
         </SessionProvider>
     );
 };
