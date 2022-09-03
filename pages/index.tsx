a6c98463a202501e737ecce84a1d347483e02ecd
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-03 16:26:30 +1200

    Create top tracks page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
 import type { NextPage } from 'next';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import Head from 'next/head';
-import { ITopTracksDTO } from '../lib/interfaces/spotify';
-import { getTopTracks } from '../lib/spotify';
 
 const Home: NextPage = () => {
     const { data: session } = useSession();
-
-    const {
-        data: topTracks,
-        isError,
-        isLoading,
-    } = useQuery<ITopTracksDTO>(['topTracks'], getTopTracks);
 
     return (
         <>
@@ -21,17 +12,6 @@ const Home: NextPage = () => {
                 <meta name="description" content="Spotifly app" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            {isLoading && <div>Loading...</div>}
-            {isError && <div>An error occured.</div>}
-            {topTracks && (
-                <ol>
-                    {topTracks.items.map((track) => (
-                        <li key={track.id}>
-                            {track.name} — {track.artists[0].name}
-                        </li>
-                    ))}
-                </ol>
-            )}
             {session ? (
                 <button onClick={() => signOut()}>Sign out</button>
             ) : (
---
+++ pages/top-tracks.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { ITopTracksDTO } from '../lib/interfaces/spotify';
+import { getTopTracks } from '../lib/spotify';
+
+const TopTracks: NextPage = () => {
+    const {
+        data: topTracks,
+        isError,
+        isLoading,
+    } = useQuery<ITopTracksDTO>(['topTracks'], getTopTracks);
+
+    return (
+        <>
+            <Head>
+                <title>Top Tracks</title>
+                <meta name="description" content="Spotifly: Top Tracks" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {isLoading && <div>Loading...</div>}
+            {isError && <div>An error occured.</div>}
+            {topTracks && (
+                <ol>
+                    {topTracks.items.map((track) => (
+                        <li key={track.id}>
+                            {track.name} — {track.artists[0].name}
+                        </li>
+                    ))}
+                </ol>
+            )}
+        </>
+    );
+};
+
+export default TopTracks;
