20b68f8fa42f69180f72a56c09b20edd9d7eeb70
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-04 19:52:33 +1200

    Add responsive meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/global.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const queryClient = new QueryClient();
 
@@ -12,12 +13,20 @@ const MyApp = ({
     pageProps: { session, ...pageProps },
 }: AppProps) => {
     return (
-        <SessionProvider session={session}>
-            <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
-        </SessionProvider>
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <SessionProvider session={session}>
+                <QueryClientProvider client={queryClient}>
+                    <Component {...pageProps} />
+                    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+                </QueryClientProvider>
+            </SessionProvider>
+        </>
     );
 };
 
