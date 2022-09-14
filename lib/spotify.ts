df2d817ae0b16e4209d8fa4c3f94aab3d8abd655
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-14 21:06:39 +1200

    Rename top tracks API route

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { ITopTracksDTO } from '../lib/interfaces/spotify';
 
 export const getTopTracks = async (): Promise<ITopTracksDTO> => {
-    const { data } = await axios.get('/api/spotify/topTracks');
+    const { data } = await axios.get('/api/spotify/getTopTracks');
     return data;
 };
