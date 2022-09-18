fc32ec038781b235d47a1c19aea57d9f6b185497
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-18 18:33:05 +1200

    Update Panel styling

--- components/Panels/PanelHeading/PanelHeading.tsx
+++ components/Panels/PanelHeading/PanelHeading.tsx
@@ -1,14 +1,18 @@
+import classNames from 'classnames';
 import { FC } from 'react';
 import styles from './PanelHeading.module.scss';
 
 export interface IPanelHeading {
-    heading: string;
-    subheading?: string;
+    title: string;
+    heading?: string;
+    large?: true;
 }
 
-export const PanelHeading: FC<IPanelHeading> = ({ heading, subheading }) => (
+export const PanelHeading: FC<IPanelHeading> = ({ title, heading, large }) => (
     <header className={styles.wrapper}>
-        <h2 className={styles.heading}>{heading}</h2>
-        {subheading && <span className={styles.subheading}>{subheading}</span>}
+        {heading && <div className={styles.heading}>{heading}</div>}
+        <h2 className={classNames(styles.title, large && styles.large)}>
+            {title}
+        </h2>
     </header>
 );
