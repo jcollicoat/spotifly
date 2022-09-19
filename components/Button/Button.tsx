3dc8aa38beac187d95b7b71f3ca0582f71fc1746
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-19 22:38:21 +1200

    Better buttons

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,19 +1,86 @@
+import classNames from 'classnames';
+import Link from 'next/link';
 import { FC } from 'react';
 import styles from './Button.module.scss';
 
-interface IButton {
+interface IShared {
     ariaLabel: string;
-    onClick: () => void;
-    text: string;
+    children: React.ReactNode;
+    style?: 'primary' | 'secondary' | 'tertiary';
 }
 
-export const Button: FC<IButton> = ({ ariaLabel, onClick, text }) => (
+interface IButtonType extends IShared {
+    onClick: () => void;
+}
+
+const ButtonType: FC<IButtonType> = ({
+    ariaLabel,
+    children,
+    onClick,
+    style = 'primary',
+}) => (
     <button
         aria-label={ariaLabel}
-        className={styles.button}
+        className={classNames(styles.button, styles[style])}
         onClick={onClick}
         type="button"
     >
-        {text}
+        {children}
     </button>
 );
+
+interface ILinkType extends IShared {
+    href: string;
+}
+
+const LinkType: FC<ILinkType> = ({
+    ariaLabel,
+    children,
+    href,
+    style = 'primary',
+}) => (
+    <Link href={href} passHref>
+        <a
+            aria-label={ariaLabel}
+            className={classNames(styles.button, styles[style])}
+        >
+            {children}
+        </a>
+    </Link>
+);
+
+interface IButtonTypeProps extends IShared {
+    onClick: () => void;
+    href?: never;
+    type?: never;
+}
+
+interface ILinkTypeProps extends IShared {
+    href: string;
+    type: 'link';
+    onClick?: never;
+}
+
+type IButtonProps = IButtonTypeProps | ILinkTypeProps;
+
+export const Button: FC<IButtonProps> = ({
+    ariaLabel,
+    children,
+    href,
+    onClick,
+    style,
+    type,
+}) => {
+    if (type === 'link') {
+        return (
+            <LinkType ariaLabel={ariaLabel} href={href} style={style}>
+                {children}
+            </LinkType>
+        );
+    }
+    return (
+        <ButtonType ariaLabel={ariaLabel} onClick={onClick} style={style}>
+            {children}
+        </ButtonType>
+    );
+};
