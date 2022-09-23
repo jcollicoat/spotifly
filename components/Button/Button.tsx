5981c04065f66cce10857308eae3b81ee787b411
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-24 11:15:05 +1200

    Support mobile glyphs in buttons

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,9 @@ import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { FC } from 'react';
+import { HiLogin, HiLogout } from 'react-icons/hi';
+import { useMedia } from 'react-use';
+import { breakpoints } from '../../context/breakpoints/breakpoints';
 import { signInOrOut } from '../../lib/auth';
 import styles from './Button.module.scss';
 
@@ -12,6 +15,7 @@ interface IButton {
 interface IButtonWithChildren extends IButton {
     ariaLabel: string;
     children: React.ReactNode;
+    smallGlyph?: React.ReactNode;
 }
 
 interface IButtonType extends IButtonWithChildren {
@@ -54,19 +58,6 @@ const LinkType: FC<ILinkType> = ({
     </Link>
 );
 
-const SignInOutType: FC<IButton> = ({ style }) => {
-    const { data: session } = useSession();
-    return (
-        <ButtonType
-            ariaLabel={session ? 'Sign out' : 'Sign in with Spotify'}
-            onClick={signInOrOut}
-            style={style ?? (session ? 'secondary' : 'primary')}
-        >
-            {session ? 'Sign out' : 'Sign in with Spotify'}
-        </ButtonType>
-    );
-};
-
 interface IButtonTypeProps extends IButtonWithChildren {
     onClick: () => void;
     href?: never;
@@ -79,36 +70,53 @@ interface ILinkTypeProps extends IButtonWithChildren {
     onClick?: never;
 }
 
-interface ISignInOutTypeProps extends IButton {
-    ariaLabel?: never;
-    children?: never;
-    type: 'signInOut';
-    onClick?: never;
-    href?: never;
-}
-
-type IButtonProps = IButtonTypeProps | ILinkTypeProps | ISignInOutTypeProps;
+type IButtonProps = IButtonTypeProps | ILinkTypeProps;
 
 export const Button: FC<IButtonProps> = ({
     ariaLabel,
     children,
     href,
     onClick,
+    smallGlyph,
     style,
     type,
 }) => {
-    if (type === 'signInOut') {
-        return <SignInOutType style={style} />;
-    } else if (type === 'link') {
+    const isSmall = useMedia(`(max-width: ${breakpoints.medium - 1}px)`);
+    const showSmallGlyph = isSmall && Boolean(smallGlyph);
+
+    if (type === 'link') {
         return (
             <LinkType ariaLabel={ariaLabel} href={href} style={style}>
-                {children}
+                {showSmallGlyph ? smallGlyph : children}
             </LinkType>
         );
     }
     return (
         <ButtonType ariaLabel={ariaLabel} onClick={onClick} style={style}>
-            {children}
+            {showSmallGlyph ? smallGlyph : children}
         </ButtonType>
+    );
+};
+
+interface ISignInOutProps extends IButton {
+    showSmallGlyph?: boolean;
+}
+
+export const ButtonSignInOut: FC<ISignInOutProps> = ({
+    showSmallGlyph = false,
+    style,
+}) => {
+    const { data: session } = useSession();
+    const smallGlyph = showSmallGlyph && (session ? <HiLogout /> : <HiLogin />);
+
+    return (
+        <Button
+            ariaLabel={session ? 'Sign out' : 'Sign in with Spotify'}
+            onClick={signInOrOut}
+            smallGlyph={smallGlyph}
+            style={style ?? (session ? 'secondary' : 'primary')}
+        >
+            {session ? 'Sign out' : 'Sign in with Spotify'}
+        </Button>
     );
 };
