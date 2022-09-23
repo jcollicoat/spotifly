eef543b99f4bf9ff99b658012f77177d33f5e38e
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-24 02:15:07 +1200

    Smart overflowing

--- components/TopTrack/TopTrack.tsx
+++ components/TopTrack/TopTrack.tsx
@@ -1,50 +1,84 @@
+import classNames from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { useWindowSize } from 'react-use';
 import { ITrackDTO } from '../../lib/interfaces/spotify';
 import styles from './TopTrack.module.scss';
 
-export const TopTrack: FC<{ track: ITrackDTO }> = ({ track }) => (
-    <div
-        className={styles.track}
-        style={{
-            backgroundImage: `url(${track.album.images[0].url})`,
-        }}
-    >
-        <div className={styles.content}>
-            <div className={styles.cover}>
-                <Image
-                    src={track.album.images[2].url}
-                    alt=""
-                    height={36}
-                    width={36}
-                />
-            </div>
-            <div className={styles.details}>
-                <div className={styles.nowrap}>
-                    <Link href={`/track/${track.id}`} passHref>
-                        <a
-                            aria-label={`Explore ${track.name} by ${track.artists[0].name}`}
-                            className={styles.name}
-                        >
-                            {track.name}
-                        </a>
-                    </Link>
-                    <div className={styles.subdetails}>
-                        <Link href={`/artist/${track.artists[0].id}`} passHref>
-                            <a className={styles.subdetail}>
-                                {track.artists[0].name}
+export const TopTrack: FC<{ track: ITrackDTO }> = ({ track }) => {
+    const detailsRef = useRef<HTMLDivElement>(null);
+    const noWrapRef = useRef<HTMLDivElement>(null);
+    const { width } = useWindowSize();
+    const [isOverflowed, setIsOverflowed] = useState(false);
+
+    const measureOverflow = (): void => {
+        const detailsWidth = detailsRef.current?.clientWidth;
+        const noWrapWidth = noWrapRef.current?.clientWidth;
+
+        if (!detailsWidth || !noWrapWidth) {
+            setIsOverflowed(false);
+        } else if (detailsWidth > noWrapWidth) {
+            setIsOverflowed(false);
+        } else if (detailsWidth < noWrapWidth) {
+            setIsOverflowed(true);
+        }
+    };
+
+    useEffect(() => {
+        measureOverflow();
+    }, [width]);
+
+    return (
+        <div
+            className={styles.track}
+            style={{
+                backgroundImage: `url(${track.album.images[0].url})`,
+            }}
+        >
+            <div
+                className={classNames(
+                    styles.content,
+                    isOverflowed && styles.overflowed
+                )}
+            >
+                <div className={styles.cover}>
+                    <Image
+                        src={track.album.images[2].url}
+                        alt=""
+                        height={36}
+                        width={36}
+                    />
+                </div>
+                <div className={styles.details} ref={detailsRef}>
+                    <div className={styles.nowrap} ref={noWrapRef}>
+                        <Link href={`/track/${track.id}`} passHref>
+                            <a
+                                aria-label={`Explore ${track.name} by ${track.artists[0].name}`}
+                                className={styles.name}
+                            >
+                                {track.name}
                             </a>
                         </Link>
-                        •
-                        <Link href={`/album/${track.album.id}`} passHref>
-                            <a className={styles.subdetail}>
-                                {track.album.name}
-                            </a>
-                        </Link>
+                        <div className={styles.subdetails}>
+                            <Link
+                                href={`/artist/${track.artists[0].id}`}
+                                passHref
+                            >
+                                <a className={styles.subdetail}>
+                                    {track.artists[0].name}
+                                </a>
+                            </Link>
+                            •
+                            <Link href={`/album/${track.album.id}`} passHref>
+                                <a className={styles.subdetail}>
+                                    {track.album.name}
+                                </a>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
