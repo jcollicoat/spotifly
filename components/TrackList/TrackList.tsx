18fe2090c33460aef687434cdb33161796eb36bf
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-26 03:00:31 +1300

    Clean up track list

--- components/TrackList/TrackList.tsx
+++ components/TrackList/TrackList.tsx
@@ -1,42 +1,12 @@
 import { FC } from 'react';
 import { useRecentlyPlayed, useTopTracks } from '../../hooks/useSpotify';
-import {
-    IRecentlyPlayed,
-    ITopTracks,
-    ITrack,
-} from '../../lib/interfaces/spotify';
+import { ITrack } from '../../lib/interfaces/spotify';
 import { IPanelDisplay, Panel } from '../Panels/Panel/Panel';
 import { IPanelHeading } from '../Panels/PanelHeading/PanelHeading';
 import { Spinner } from '../Spinner/Spinner';
 import { Track } from './Track/Track';
 
 type ComponentTypes = 'recently-played' | 'top-tracks';
-
-type DataTypes = IRecentlyPlayed | ITopTracks;
-
-interface IList<T> {
-    items: T[];
-}
-
-const RecentlyPlayed: FC<IList<ITrack>> = ({ items }) => {
-    return (
-        <>
-            {items.map((item) => (
-                <Track key={item.unique_id} track={item} />
-            ))}
-        </>
-    );
-};
-
-const TopTracks: FC<IList<ITrack>> = ({ items }) => {
-    return (
-        <>
-            {items.map((item) => (
-                <Track key={item.unique_id} track={item} />
-            ))}
-        </>
-    );
-};
 
 interface ITracksList {
     subheading: string;
@@ -56,14 +26,8 @@ export const TrackList: FC<ITracksList> = ({ subheading, title, type }) => {
     const query = mapQueryType();
     const { data, isError, isLoading } = query();
 
-    const mapTracks = (data: DataTypes) => {
-        switch (type) {
-            case 'recently-played':
-                return <RecentlyPlayed items={data.items as ITrack[]} />;
-            case 'top-tracks':
-                return <TopTracks items={data.items as ITrack[]} />;
-        }
-    };
+    const mapTracks = (tracks: ITrack[]) =>
+        tracks.map((track) => <Track key={track.unique_id} track={track} />);
 
     const heading: IPanelHeading = {
         title: title,
@@ -80,7 +44,7 @@ export const TrackList: FC<ITracksList> = ({ subheading, title, type }) => {
         <Panel display={display} heading={heading}>
             {isLoading && <Spinner padding="small" />}
             {isError && <div>An error occured.</div>}
-            {data && mapTracks(data)}
+            {data && mapTracks(data.items)}
         </Panel>
     );
 };
