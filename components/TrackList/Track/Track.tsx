2718de50aab2b7dd3f266cca7d6dcdee4f1aa9a6
Joseph Collicoat <jcollicoat@gmail.com>
2022-09-30 00:07:30 +1300

    TrackSkeleton pure component

--- components/TrackList/Track/Track.tsx
+++ components/TrackList/Track/Track.tsx
@@ -1,21 +1,47 @@
 import { useColor } from 'color-thief-react';
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { useWindowSize } from 'react-use';
 import { ITrack } from '../../../lib/interfaces/spotify';
 import { TrackSkeleton } from './TrackSkeleton';
 
 export const Track: FC<{ track: ITrack }> = ({ track }) => {
+    const detailsRef = useRef<HTMLDivElement>(null);
+    const noWrapRef = useRef<HTMLDivElement>(null);
+    const { width } = useWindowSize();
+    const [isOverflowed, setIsOverflowed] = useState(false);
+
+    const measureOverflow = (): void => {
+        const detailsWidth = detailsRef.current?.clientWidth;
+        const noWrapWidth = noWrapRef.current?.clientWidth;
+
+        if (!detailsWidth || !noWrapWidth) {
+            setIsOverflowed(false);
+        } else if (detailsWidth > noWrapWidth) {
+            setIsOverflowed(false);
+        } else if (detailsWidth < noWrapWidth) {
+            setIsOverflowed(true);
+        }
+    };
+
+    useEffect(() => {
+        measureOverflow();
+    }, [width]);
+
     const { data: albumColor } = useColor(track.album.image, 'hex', {
         crossOrigin: 'true',
     });
 
     const data = {
-        albumColor: albumColor,
         track: {
             album: track.album,
             artists: track.artists,
             id: track.id,
             name: track.name,
         },
+        detailsRef: detailsRef,
+        noWrapRef: noWrapRef,
+        albumColor: albumColor,
+        isOverflowed: isOverflowed,
     };
 
     return <TrackSkeleton data={data} />;
--- components/TrackList/Track/TrackSkeleton.tsx
+++ components/TrackList/Track/TrackSkeleton.tsx
@@ -1,8 +1,7 @@
 import classNames from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FC, useEffect, useRef, useState } from 'react';
-import { useWindowSize } from 'react-use';
+import { FC } from 'react';
 import { ITrack } from '../../../lib/interfaces/spotify';
 import { IComponent } from '../../interfaces';
 import { SkeletonImage } from '../../Skeletons/SkeletonImage/SkeletonImage';
@@ -11,115 +10,90 @@ import styles from './Track.module.scss';
 
 interface ITrackLoaded {
     track: Pick<ITrack, 'album' | 'artists' | 'id' | 'name'>;
+    detailsRef: React.RefObject<HTMLDivElement>;
+    noWrapRef: React.RefObject<HTMLDivElement>;
     albumColor?: string;
+    isOverflowed?: boolean;
 }
 
 type TrackSkeleton = IComponent<ITrackLoaded>;
 
-export const TrackSkeleton: FC<TrackSkeleton> = ({ data, state }) => {
-    const detailsRef = useRef<HTMLDivElement>(null);
-    const noWrapRef = useRef<HTMLDivElement>(null);
-    const { width } = useWindowSize();
-    const [isOverflowed, setIsOverflowed] = useState(false);
-
-    const measureOverflow = (): void => {
-        const detailsWidth = detailsRef.current?.clientWidth;
-        const noWrapWidth = noWrapRef.current?.clientWidth;
-
-        if (!detailsWidth || !noWrapWidth) {
-            setIsOverflowed(false);
-        } else if (detailsWidth > noWrapWidth) {
-            setIsOverflowed(false);
-        } else if (detailsWidth < noWrapWidth) {
-            setIsOverflowed(true);
-        }
-    };
-
-    useEffect(() => {
-        measureOverflow();
-    }, [width]);
-
-    return (
+export const TrackSkeleton: FC<TrackSkeleton> = ({ data, state }) => (
+    <div
+        className={styles.track}
+        style={{
+            backgroundColor: data && data.albumColor,
+        }}
+    >
         <div
-            className={styles.track}
-            style={{
-                backgroundColor: data && data.albumColor,
-            }}
+            className={classNames(
+                styles.content,
+                data && data.isOverflowed && styles.overflowed
+            )}
         >
-            <div
-                className={classNames(
-                    styles.content,
-                    isOverflowed && styles.overflowed
+            <div className={styles.cover}>
+                {data ? (
+                    <Image
+                        src={data.track.album.image}
+                        alt=""
+                        height={36}
+                        width={36}
+                    />
+                ) : (
+                    <SkeletonImage height="36px" width="36px" state={state} />
                 )}
-            >
-                <div className={styles.cover}>
-                    {data ? (
-                        <Image
-                            src={data.track.album.image}
-                            alt=""
-                            height={36}
-                            width={36}
-                        />
-                    ) : (
-                        <SkeletonImage
-                            height="36px"
-                            width="36px"
-                            state={state}
-                        />
-                    )}
-                </div>
-                <div className={styles.details} ref={detailsRef}>
-                    <div className={styles.nowrap} ref={noWrapRef}>
-                        <a aria-hidden className={styles.name}>
-                            {data ? (
-                                <Link href={`/track/${data.track.id}`} passHref>
-                                    <a
-                                        aria-label={`Explore ${data.track.name} by ${data.track.artists[0].name}`}
-                                        className={styles.name}
-                                    >
-                                        {data.track.name}
-                                    </a>
-                                </Link>
-                            ) : (
-                                <a aria-hidden className={styles.name}>
-                                    <SkeletonText state={state} />
-                                </a>
-                            )}
-                        </a>
-                        <div className={styles.subdetails}>
-                            {data ? (
-                                <Link
-                                    href={`/artist/${data.track.artists[0].id}`}
-                                    passHref
+            </div>
+            <div className={styles.details} ref={data && data.detailsRef}>
+                <div className={styles.nowrap} ref={data && data.noWrapRef}>
+                    <a aria-hidden className={styles.name}>
+                        {data ? (
+                            <Link href={`/track/${data.track.id}`} passHref>
+                                <a
+                                    aria-label={`Explore ${data.track.name} by ${data.track.artists[0].name}`}
+                                    className={styles.name}
                                 >
-                                    <a className={styles.subdetail}>
-                                        {data.track.artists[0].name}
-                                    </a>
-                                </Link>
-                            ) : (
-                                <a className={styles.subdetail}>
-                                    <SkeletonText state={state} />
+                                    {data.track.name}
                                 </a>
-                            )}
-                            •
-                            {data ? (
-                                <Link
-                                    href={`/album/${data.track.album.id}`}
-                                    passHref
-                                >
-                                    <a className={styles.subdetail}>
-                                        {data.track.album.name}
-                                    </a>
-                                </Link>
-                            ) : (
+                            </Link>
+                        ) : (
+                            <a aria-hidden className={styles.name}>
+                                <SkeletonText state={state} />
+                            </a>
+                        )}
+                    </a>
+                    <div className={styles.subdetails}>
+                        {data ? (
+                            <Link
+                                href={`/artist/${data.track.artists[0].id}`}
+                                passHref
+                            >
                                 <a className={styles.subdetail}>
-                                    <SkeletonText state={state} />
+                                    {data.track.artists[0].name}
                                 </a>
-                            )}
-                        </div>
+                            </Link>
+                        ) : (
+                            <a className={styles.subdetail}>
+                                <SkeletonText state={state} />
+                            </a>
+                        )}
+                        •
+                        {data ? (
+                            <Link
+                                href={`/album/${data.track.album.id}`}
+                                passHref
+                            >
+                                <a className={styles.subdetail}>
+                                    {data.track.album.name}
+                                </a>
+                            </Link>
+                        ) : (
+                            <a className={styles.subdetail}>
+                                <SkeletonText state={state} />
+                            </a>
+                        )}
                     </div>
                 </div>
             </div>
         </div>
-    );
-};
+    </div>
+);
