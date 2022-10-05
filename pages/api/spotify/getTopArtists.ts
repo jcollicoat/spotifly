95ebd2347196145e40883348acb32d66ca7eea88
Joseph Collicoat <jcollicoat@gmail.com>
2022-10-05 21:19:17 +1300

    getTopArtists in Postman

--- pages/api/spotify/getTopArtists.ts
+++ pages/api/spotify/getTopArtists.ts
@@ -1,29 +1,38 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getSession } from 'next-auth/react';
+import { determineAccessToken } from '../../../lib/server/auth';
 import { buildTopArtists } from '../../../lib/server/spotify';
+import { ITopArtistsDTO } from '../../../lib/server/spotify-types';
 
 const endpoint = 'https://api.spotify.com/v1/me/top/artists';
 
+const getTopArtists = async (
+    req: NextApiRequest
+): Promise<AxiosResponse<ITopArtistsDTO> | null> => {
+    const access_token = await determineAccessToken(req);
+    if (access_token === null) {
+        return access_token;
+    }
+
+    return await axios.get<ITopArtistsDTO>(endpoint, {
+        headers: {
+            Authorization: access_token,
+        },
+    });
+};
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const session = await getSession({ req });
+    const api = await getTopArtists(req);
 
-    if (!session) {
-        res.status(401).send(
-            'No session data found. User is likely not logged in.'
-        );
+    if (!api) {
+        res.status(401).send('Invalid Spotify access_token provided.');
     } else {
-        const access_token = session.access_token;
-
-        const response = await axios.get(endpoint, {
-            headers: {
-                Authorization: `Bearer ${access_token}`,
-            },
-        });
-        const built = await buildTopArtists(response.data);
-
-        res.status(response.status).json(built);
+        if (api.status !== 200) {
+            res.status(api.status).json(api.data);
+        }
+        const built = await buildTopArtists(api.data);
+        res.status(200).json(built);
     }
 };
 
